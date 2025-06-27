9eba78dcc65d716379e23be6ada0067c0784cdf0
Alex Aquino <aaquino1223@gmail.com>
2025-06-27 17:15:06 -0400

    Added GET Encounter Locations endpoint Added unit tests Added integration tests Updated CSharp client

---
+++ edudoc-v5/backend/EduDoc.Api/Endpoints/EncounterLocations/Models/EncounterLocationResponseModel.cs
@@ -0,0 +1,7 @@
+namespace EduDoc.Api.Endpoints.EncounterLocations.Models;
+
+public class EncounterLocationResponseModel
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+} 
